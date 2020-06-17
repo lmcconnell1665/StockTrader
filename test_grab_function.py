ce403017f02f973202e7f86cc65105ac5531d49e
Luke McConnell <56060247+lmcconnell1665@users.noreply.github.com>
2020-06-16 19:04:06 -0500

    Delete test_grab_function.py
    Combined into test_function.py

--- test_grab_function.py
+++
@@ -1,8 +0,0 @@
-from click.testing import CliRunner
-from stonk import grab
-
-def test_grab_function():
-    runner = CliRunner()
-    result = runner.invoke(grab, ["--ticker", "SNAP"])
-    assert result.exit_code == 0 # tests to make sure function returns an exit code of 0
-    # assert(result.output) == '200\n' # tests to make sure the function (which returns api status_code) returns 200
